14b2ae0a6d7e8a779d280793e5ab61ff3a4962f1
Elias Delgado <evdelgado99@gmail.com>
2020-08-06 02:39:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,16 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
+puts "A Tic Tac Toe Board"
+  
+
 def display_board
-  puts "A Tic Tac Toe Board"
-  do  
+
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  end
+  
 end
 
 display_board
